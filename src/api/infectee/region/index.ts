a5ef5962a68b1bcab1c56f2a8c661e39265b6add
choi jinwoo <chlwlsdn0828@gmail.com>
2020-02-26 16:06:14 +0900

    [ADD] Apply ModifyRegionInfectee AuthMiddleware

--- src/api/infectee/region/index.ts
+++ src/api/infectee/region/index.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import getRegionInfectee from './region.ctrl/getRegionInfectee';
 import modifyRegionInfectee from './region.ctrl/modifyRegionInfectee';
+import auth from '../../../lib/middleware/auth';
 
 const router = Router();
 
 router.get('/', getRegionInfectee);
-router.put('/', modifyRegionInfectee);
+router.put('/', auth, modifyRegionInfectee);
 
 export default router;
