6ccbe726c71347f88a4cd1737c2c4b3f4c8deb90
choi jinwoo <chlwlsdn0828@gmail.com>
2020-03-01 17:50:15 +0900

    [FIX] Get Nation Infectee Response

--- src/api/infectee/nation/nation.ctrl/getNationInfectee.ts
+++ src/api/infectee/nation/nation.ctrl/getNationInfectee.ts
@@ -3,11 +3,18 @@ import nationalState from '../../../../../data/nationalState.json';
 
 export default async (req: Request, res: Response) => {
   let nationData = nationalState;
-
+  const result = [];
+  const nationDataKey = Object.keys(nationData);
+  nationDataKey.forEach((key) => {
+    result.push({
+      nation: key,
+      number: nationData[key],
+    });
+  })
   res.status(200).json({
     message: '국가별 감염자 조회 성공.',
     data: {
-      nation_infectee: nationData,
+      nation_infectee: result,
     },
   });
 }
